a93d084c6f15209f2a6dc43be0d90de0eb07a140
Chetan <atcs.chetan@mbusa.com>
2017-06-28 12:42:34 -0400

    inital commit on master

---
+++ TestJavaProject/src/com/atcs/pac1/HelloJava.java
@@ -0,0 +1,6 @@
+package com.atcs.pac1;
+
+public class HelloJava {
+// This comment is added newly
+// added in master
+}
---
+++ TestJavaProject/src/com/atcs/pac2/TakeCareJava.java
@@ -0,0 +1,6 @@
+package com.atcs.pac2;
+
+public class TakeCareJava {
+// This comment was originally sdded in the remote copy of the sourceFile
+  // This comment was originally sdded in the remote copy of the sourceFile
+}
